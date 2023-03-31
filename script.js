804096c7d9eb94d6fef3f417d1030321bfed20c4
casperrr <casper.sebastien@gmail.com>
2023-03-31 13:03:21 +0100

    cleaned Code

--- script.js
+++ script.js
@@ -2,33 +2,15 @@ const canvas = document.getElementById("canvas");
 const c = canvas.getContext('2d');
 
 // Globals
-let points = [];
-let angle = 0;
-let scale = 300; //tip: when using perspective use distance*100
-let distance = 3;
 let cube;
 let render;
 
-//Matrixes:
-let orthoProjMat = [
-    [1,0,0],
-    [0,1,0]
-];
-
-function usePerspective(matrix,z){
-    for(let i = 0; i < matrix.length; i++){
-        for(let j = 0; j < matrix[0].length; j++){
-            matrix[i][j] != 0 ? matrix[i][j] = z : matrix[i][j] = 0;
-        }
-    }
-}
 
 
 function init(){
     c.translate(canvas.width/2,canvas.height/2);
-    //points = makeCube();
     cube = new Cube();
-    render = new Render3D(false,5);
+    render = new Render3D(false,4);
     cube.makeCube();
 }
 
@@ -42,123 +24,17 @@ function loop(){
 
     render.drawShape(cube);
 
-    cube.xrot += 0.01;
+    //cube.xrot += 0.01;
     cube.yrot += 0.01;
 
-
-    // let rotArr = points;
-    // //rotArr = rotX(angle,rotArr);
-    // rotArr = rotY(angle,rotArr);
-    // //rotArr = rotZ(angle,rotArr);
-
-
-    // arr = aplyMat(orthoProjMat,rotArr);
-    // arr.forEach(i => i.scalar(scale)); //Scale Points Up
-    // //draw(arr);
-    // conCube(arr);
-
-    // angle += 0.01;
     requestAnimationFrame(loop);
 }
 
-function draw(arr){
-    for(let i = 0; i < points.length;i++){
-        drawPoint(arr[i].mat[0][0],arr[i].mat[1][0],5);
-        
-    }
-}
-
-
-function drawPoint(x,y,r){
-    c.fillStyle = '#ffffff';
-    c.beginPath();
-    c.arc(x,y,r,0,2*Math.PI);
-    c.fill();
-    c.closePath();
-}
-
-function conCube(arr){
-    //Only works for cube!
-    let order = [0,1,3,2]
-    for(let i = 0; i < 4; i++){
-        connectPnts(arr,arr[order[i]],arr[order[(i+1)%4]]);
-        connectPnts(arr,arr[order[i]+4],arr[order[(i+1)%4]+4]);
-        connectPnts(arr,arr[i],arr[i+4]);
-    }
-}
-
-function connectPnts(arr,a,b){
-    c.strokeStyle = '#ffffff';
-    c.lineWidth = 2;
-    c.beginPath();
-    c.moveTo(a.mat[0],a.mat[1]);
-    c.lineTo(b.mat[0],b.mat[1]);
-    c.stroke();
-}
 
 function make2D(rows,cols){
     let arr = new Array(rows).fill(0).map(() => new Array(cols).fill(0));
     return arr;
 }
-
-function makeCube(){
-    let arr = [];
-    for(let i = 0; i < 8; i++){
-        arr.push(new Matrix(3,1));
-        arr[i].mat = [
-            [(i&1) == 0 ? -1 : 1],
-            [(i&2) == 0 ? -1 : 1],
-            [(i&4) == 0 ? -1 : 1]];
-    }
-    return arr;
-}
-
-function aplyMat(matrix, arr){
-    let newArr = [];
-    for(let i = 0; i < arr.length; i++){
-        let z = 1/(distance-arr[i].mat[2]);
-        usePerspective(matrix,z)
-        newArr[i] = arr[i].dotProd(matrix);
-    }
-    return newArr;
-}
-
-function rotX(ang,arr){
-    let rXmat = [
-        [1,0,0],
-        [0,Math.cos(ang),-Math.sin(ang)],
-        [0,Math.sin(ang),Math.cos(ang)]
-    ];
-    return applyRotMat(rXmat,arr);
-}
-
-function rotY(ang,arr){
-    let rYmat = [
-        [Math.cos(ang),0,Math.sin(ang)],
-        [0,1,0],
-        [-Math.sin(ang),0,Math.cos(ang)]
-    ];
-    return applyRotMat(rYmat,arr);
-}
-
-function rotZ(ang,arr){
-    let rZmat = [
-        [Math.cos(ang),-Math.sin(ang),0],
-        [Math.sin(ang),Math.cos(ang),0],
-        [0,0,1]
-    ];
-    return applyRotMat(rZmat,arr);
-}
-
-
-function applyRotMat(matrix,arr){
-    let newArr = [];
-    for(let i = 0; i < arr.length; i++){
-        newArr[i] = arr[i].dotProd(matrix);
-    }
-    return newArr;
-} 
-
 
 class Matrix{
     constructor(rows, cols){
@@ -192,7 +68,7 @@ class Cube{
         this.xrot = 0;
         this.yrot = 0;
         this.zrot = 0;
-        this.scl = 100;
+        this.scl = 300;
         //this.pos = ima not worry about this for now. 
     }
 
@@ -265,8 +141,9 @@ class Render3D{
 
     #projectShape(shape){
         let projShape = shape;
-        projShape.forEach((i)=>{
-            i=this.#projectPoint(i);
+        projShape.forEach((mat,i)=>{
+            let x=this.#projectPoint(mat);
+            projShape[i] = x;
         });
         return projShape;
     }
